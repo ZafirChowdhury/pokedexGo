bc209fbb409d73b619030c808cebb2a88ba303fd
zafir <zafirchowdhury69@gmail.com>
2026-05-13 15:50:45 +0600

    Refactor

--- repl.go
+++ repl.go
@@ -7,22 +7,6 @@ import (
 	"strings"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-func cleanInput(text string) []string {
-	trimmed := strings.TrimSpace(text)
-
-	lowered := strings.ToLower(trimmed)
-
-	words := strings.Fields(lowered)
-
-	return words
-}
-
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -50,6 +34,22 @@ func startRepl() {
 	}
 }
 
+func cleanInput(text string) []string {
+	trimmed := strings.TrimSpace(text)
+
+	lowered := strings.ToLower(trimmed)
+
+	words := strings.Fields(lowered)
+
+	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func getCommands() map[string]cliCommand {
 	return map[string]cliCommand{
 		"exit": {
@@ -64,25 +64,4 @@ func getCommands() map[string]cliCommand {
 			callback:    commandHelp,
 		},
 	}
-}
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Println()
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:")
-	fmt.Println()
-
-	for _, cmd := range getCommands() {
-		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
-	}
-	fmt.Println()
-
-	return nil
 }
